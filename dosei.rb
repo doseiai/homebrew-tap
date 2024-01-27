061f5b9c96d03b210bf84127f2e6d7caf39ac724
Alw3ys <alw3ys@gmail.com>
2024-01-27 02:13:04 +0100

    update sha

--- dosei.rb
+++ dosei.rb
@@ -2,5 +2,5 @@ class Dosei < Formula
   desc "Developer Platform for creating, deploying, and managing containerized applications."
   homepage "https://dosei.ai"
   url "https://github.com/doseiai/engine/archive/refs/tags/0.0.9.tar.gz"
-  sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+  sha256 "4f3294d351498f34821bfaf3fc938a922e68bb76c2676be9fbb8ab15e4759c45"
   license "Apache-2.0"
