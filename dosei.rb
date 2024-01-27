fd98a88286e605218b560207be9fd5ee190d3c2e
Alw3ys <alw3ys@gmail.com>
2024-01-27 02:05:15 +0100

    init

---
+++ dosei.rb
@@ -0,0 +1,6 @@
+class Dosei < Formula
+  desc "Developer Platform for creating, deploying, and managing containerized applications."
+  homepage "https://dosei.ai"
+  url "https://github.com/doseiai/engine/archive/refs/tags/0.0.9.tar.gz"
+  sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+  license "Apache-2.0"
