3024640587f892c067c424989bface7fa67c1ab4
Alw3ys <alw3ys@gmail.com>
2024-01-28 15:11:02 +0100

    chore(release): 0.0.10

--- Formula/dctl.rb
+++ Formula/dctl.rb
@@ -2,26 +2,27 @@
 # Current manual workaround:
 # Retrieve the sha256 hashes of files from this CI step:
 # https://github.com/doseiai/engine/blob/main/.github/workflows/cli-ci-cd.yaml#L84
+# Example: https://github.com/doseiai/engine/actions/runs/7686266959/job/20944978140
 
 class Dctl < Formula
   desc "Command Line Interface (CLI) for Dosei."
   homepage "https://dosei.ai"
   license "Apache-2.0"
-  version "0.0.9"
+  version "0.0.10"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/doseiai/engine/releases/download/0.0.9/dctl-x86_64-apple-darwin.zip"
-    sha256 "4efc4be0cb969308566ca8ec1f905668e8b7444e5c764784ee61fde8673fbdd7"
+    url "https://github.com/doseiai/engine/releases/download/0.0.10/dctl-x86_64-apple-darwin.zip"
+    sha256 "4ba2080df547cad741ca92167ef73dc23d2c662c702bc4c9658f23e6cd85ed97"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/doseiai/engine/releases/download/0.0.9/dctl-aarch64-apple-darwin.zip"
-    sha256 "b02445c0807cb8e1463a22e44c55c58701b8034b0d7079fcc95d0a6e8c03d0a4"
+    url "https://github.com/doseiai/engine/releases/download/0.0.10/dctl-aarch64-apple-darwin.zip"
+    sha256 "5a579310dd774f34336b6e06a08a63f5f43a68d184ec4048105411b7aff8df24"
   end
 
   if OS.linux?
-    url "https://github.com/doseiai/engine/releases/download/0.0.9/dctl-x86_64-unknown-linux-gnu.zip"
-    sha256 "90e6a1029b7d61c005c6ecad9c4733c7e9b8dbebaa4311b9ba33c0da034bf36e"
+    url "https://github.com/doseiai/engine/releases/download/0.0.10/dctl-x86_64-unknown-linux-gnu.zip"
+    sha256 "1b51653677f3a3aceed99f8f61145444372900e4fc38afd6199d2bc4642f2bb1"
   end
 
   def install
