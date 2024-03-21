2b9b89ecb1a5d8552e38efa522eca2b10237dde7
Alw3ys <alw3ys@gmail.com>
2024-03-21 23:25:49 +0100

    0.0.21

--- Formula/dosei.rb
+++ Formula/dosei.rb
@@ -1,15 +1,36 @@
-class Dosei < Formula
-  desc "Developer Platform for creating, deploying, and managing containerized applications."
-  homepage "https://dosei.ai"
-  url "https://github.com/doseiai/engine/archive/refs/tags/0.0.9.tar.gz"
-  sha256 "4f3294d351498f34821bfaf3fc938a922e68bb76c2676be9fbb8ab15e4759c45"
-  license "Apache-2.0"
+# TODO: Automate version update process.
+# Current manual workaround:
+# Retrieve the sha256 hashes of files from this CI step:
+# https://github.com/doseiai/engine/blob/main/.github/workflows/cli-ci-cd.yaml#L84
+# Example: https://github.com/doseiai/engine/actions/runs/7686266959/job/20944978140
 
-  depends_on "protobuf" => :build
-  depends_on "rust" => :build
+class Dosei < Formula
+  desc "Command Line Interface (CLI) for Dosei."
+  homepage "https://dosei.ai"
+  license "Apache-2.0"
+  version "0.0.21"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/doseiai/engine/releases/download/0.0.21/dosei-x86_64-apple-darwin.zip"
+    sha256 "05132bdb51a0bc1af3fe48ac22d075d20e9f08a21b58fa310a9b41850b54e6e0"
+  end
+
+  if OS.linux?
+    url "https://github.com/doseiai/engine/releases/download/0.0.21/dosei-x86_64-unknown-linux-gnu.zip"
+    sha256 "f44e417465a57287fc504865b7ccbdd98ea9ce1351072b44c961102738cc49b7"
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/doseiai/engine/releases/download/0.0.21/dosei-aarch64-apple-darwin.zip"
+    sha256 "ff37c3c91d36f715e8201015fe12bad619db55b1826d376ad08c201c9374ca14"
+  end
 
   def install
-    system "make"
+    bin.install "dosei"
+  end
+
+  test do
+    system "#{bin}/dosei --version"
   end
 
 end
